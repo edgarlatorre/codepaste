ccb652591cefbd06bd82cce52def4ce26a3fcb5d
edgar <edgar@spooky.local>
2010-09-30 23:10:03 -0300

    Alteracoes nos templates

--- paste/tests/test_views.py
+++ paste/tests/test_views.py
@@ -11,4 +11,24 @@ class CodeViewTest(TestCase):
 	def test_index_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'paste/index.html')
-	
+			
+	def test_should_contain_a_form(self):
+		response = self.client.get('/')
+		self.assertContains(response, '<form')
+		self.assertContains(response, '</form>')
+		
+	def test_should_contain_a_title_field(self):
+		response = self.client.get('/')
+		self.assertContains(response, "id='title'")
+		
+	def test_should_contain_a_language_field(self):
+		response = self.client.get('/')
+		self.assertContains(response, "id='language'")
+		
+	def test_should_contain_a_code_field(self):
+		response = self.client.get('/')
+		self.assertContains(response, "id='code'")
+		
+	def test_should_contain_a_submit_button(self):
+		response = self.client.get('/')
+		self.assertContains(response, "type='submit'")
--- paste/views.py
+++ paste/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render_to_response
 from codepaste.paste.models import Code
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404
+from django import forms
+
+class CodeForm(forms.Form):
+	title = forms.CharField(max_length=60)
+	language = forms.CharField(max_length=60)
+	code = forms.Field(widget=forms.Textarea)
 
 def index(request):
-	return render_to_response('paste/index.html')
+	return render_to_response('paste/index.html', locals(), context_instance=RequestContext(request))
+	
+def save(request):
+	"""docstring for save"""
+	pass
 
